2dd32f88dd0e0a34a68f8297b0c36cf0f98a79ac
BobCentP <bobCentPlatinum@outlook.com>
2025-02-04 10:32:25 +0800

    add constraintlayout

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -6,7 +6,7 @@ class CrimeListViewModel: ViewModel() {
     val crimes = mutableListOf<Crime>()
 
     init {
-        for (i in 0 until 100){
+        for (i in 1.. 100){
             val crime= Crime()
             crime.title= "Crime #$i"
             crime.isSolved= i%2==0
